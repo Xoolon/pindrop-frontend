1d889508f91be2328b8a9ee6b63077724c41680b
Xoolon <151815437+Xoolon@users.noreply.github.com>
2026-03-03 09:57:50 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useCallback, useEffect } from 'react';
 import AdOverlay from './components/AdOverlay.jsx';
 import AdBanner from './components/AdBanner.jsx';
 import { MediaCard } from './components/MediaCard.jsx';
-import { ExoClickBanner } from './components/ExoClickBanner.jsx';
+import ExoClickBanner from './components/ExoClickBanner.jsx';
 import { DownloadHistory } from './components/DownloadHistory.jsx';
 import FAQSection from './components/FAQSection.jsx';
 import { PremiumModal } from './components/PremiumModal.jsx';
